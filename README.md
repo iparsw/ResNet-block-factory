497107f93a00356132499e9e10e99309bd556720
parsa <parsaroshanak@gmail.com>
2023-04-27 03:50:19 +0430

    1.0.2
    added Multi Path Residual Block

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ![Demo1](images/photo_2023-04-26_00-22-16.jpg)
 ![Demo1](images/photo_2023-04-26_00-26-19.jpg)
 ![Demo1](images/photo_2023-04-27_00-54-41.jpg)
+![Demo1](images/photo_2023-04-27_03-22-13.jpg)
 
 
 # REFERENCES
@@ -11,3 +12,4 @@ https://arxiv.org/pdf/1512.03385.pdf
 https://arxiv.org/pdf/1906.10891.pdf
 https://arxiv.org/abs/1707.02921
 https://ieeexplore.ieee.org/document/8819933
+https://www.researchgate.net/figure/The-comparison-of-residual-blocks-in-ResNet-SRResNet-and-our-proposed-ResNet_fig1_353973396
--- ResFactory.py
+++ ResFactory.py
@@ -1,5 +1,6 @@
 from tensorflow import keras
 
+
 def ResBlock(x, nf: int = 32):
     copy = x
     x = keras.layers.Conv2D(nf, 3, padding="same")(x)
@@ -136,4 +137,22 @@ def FRPAResBlock(x, nf: int = 32,
         x = keras.layers.Conv2D(nf, 3, padding="same")(x)
     x = keras.layers.Concatenate()([x, copy2])
     x = keras.layers.Add()([x, copy1])
+    return x
+
+
+def MLResBlock(x, nf: int = 32,
+               number_of_pathes: int = 2,
+               depth: int = 1,
+               kernal_size: int = 3):
+    """Multi Path Residual Block"""
+    if nf % number_of_pathes != 0:
+        raise ValueError("Number of filters should be divisible by number of pathes")
+    copy = x
+    pathes = [x] * number_of_pathes
+    for i in range(number_of_pathes):
+        for _ in range(depth):
+            pathes[i] = keras.layers.Conv2D(nf / number_of_pathes, kernel_size=kernal_size, padding="same")(pathes[i])
+    x = keras.layers.Concatenate()(pathes)
+    x = keras.layers.LeakyReLU()(x)
+    x = keras.layers.Add()([x, copy])
     return x
